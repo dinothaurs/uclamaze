2d4b7d5581503a5138367cb7b991a44129957349
dinothaurs <janiekuang8679@gmail.com>
2025-02-20 16:33:43 -0800

    committing files

---
+++ main.js
@@ -0,0 +1,161 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+const scene = new THREE.Scene();
+
+//THREE.PerspectiveCamera( fov angle, aspect ratio, near depth, far depth );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 5, 10);
+controls.target.set(0, 5, 0);
+
+// Rendering 3D axis
+const createAxisLine = (color, start, end) => {
+    const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
+    const material = new THREE.LineBasicMaterial({ color: color });
+    return new THREE.Line(geometry, material);
+};
+const xAxis = createAxisLine(0xff0000, new THREE.Vector3(0, 0, 0), new THREE.Vector3(3, 0, 0)); // Red
+const yAxis = createAxisLine(0x00ff00, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 3, 0)); // Green
+const zAxis = createAxisLine(0x0000ff, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 3)); // Blue
+scene.add(xAxis);
+scene.add(yAxis);
+scene.add(zAxis);
+
+
+// ***** Assignment 2 *****
+// Setting up the lights
+const pointLight = new THREE.PointLight(0xffffff, 100, 100);
+pointLight.position.set(5, 5, 5); // Position the light
+scene.add(pointLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0.5, .0, 1.0).normalize();
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0x505050);  // Soft white light
+scene.add(ambientLight);
+
+const phong_material = new THREE.MeshPhongMaterial({
+    color: 0x00ff00, // Green color
+    shininess: 100   // Shininess of the material
+});
+
+
+// Start here.
+
+const l = 0.5
+const positions = new Float32Array([
+    // Front face
+    -l, -l,  l, // 0
+     l, -l,  l, // 1
+     l,  l,  l, // 2
+    -l,  l,  l, // 3
+
+    // Left face
+    -l, -l, -l, // 4
+    -l, -l,  l, // 5
+    -l,  l,  l, // 6 
+    -l,  l, -l, // 7
+  
+    // Top face
+  
+    // Bottom face
+  
+    // Right face
+
+     // Back face
+  ]);
+  
+  const indices = [
+    // Front face
+    0, 1, 2,
+    0, 2, 3,
+  
+    // Left face
+    4, 5, 6,
+    4, 6, 7,
+  
+    // Top face
+  
+    // Bottom face
+  
+    // Right face
+
+    // Back face
+  ];
+  
+  // Compute normals
+  const normals = new Float32Array([
+    // Front face
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+  
+    // Left face
+    -1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+  
+    // Top face
+  
+    // Bottom face
+  
+    // Right face
+
+    // Back face
+  ]);
+
+const custom_cube_geometry = new THREE.BufferGeometry();
+custom_cube_geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+custom_cube_geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
+custom_cube_geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices), 1));
+
+let cube = new THREE.Mesh( custom_cube_geometry, phong_material );
+scene.add(cube);
+
+// TODO: Implement wireframe geometry
+
+
+function translationMatrix(tx, ty, tz) {
+	return new THREE.Matrix4().set(
+		1, 0, 0, tx,
+		0, 1, 0, ty,
+		0, 0, 1, tz,
+		0, 0, 0, 1
+	);
+}
+// TODO: Implement the other transformation functions.
+
+
+let cubes = [];
+for (let i = 0; i < 7; i++) {
+	let cube = new THREE.Mesh(custom_cube_geometry, phong_material);
+	cube.matrixAutoUpdate = false;
+	cubes.push(cube);
+	scene.add(cube);
+}
+
+// TODO: Transform cubes
+
+
+function animate() {
+    
+	renderer.render( scene, camera );
+    controls.update();
+
+    // TODO
+    // Animate the cube
+
+}
+renderer.setAnimationLoop( animate );
+
+// TODO: Add event listener
